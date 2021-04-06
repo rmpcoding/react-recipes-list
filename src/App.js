fd76f24e45db6c338c00399772bbe33487bb51ef
Robert Parsons <rmpcoding@gmail.com>
2021-04-05 22:31:16 -0700

    Adds CSS directory and imports to app.css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RecipeList from './components/RecipeList/RecipeList';
+import './css/App.css';
 
 function App() {
     return <RecipeList recipes={sampleRecipes} />;
--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -3,11 +3,14 @@ import Recipe from '../Recipe/Recipe';
 
 const RecipeList = ({ recipes }) => {
     return (
-        <div>
-            {recipes.map((recipe) => {
-                return <Recipe key={recipe.id} {...recipe} />;
-            })}
-        </div>
+        <>
+            <div>
+                {recipes.map((recipe) => {
+                    return <Recipe key={recipe.id} {...recipe} />;
+                })}
+            </div>
+            <button>Add Recipe</button>
+        </>
     );
 };
 
